0b0b244b413ef1cfb62971d4eb3b73bf27c3199f
Volodymir Konovalov <vvkon@meta.ua>
2019-09-16 12:37:09 +0300

    Добавлен файл JSON

--- Surce.cpp
+++ Surce.cpp
@@ -1,7 +1,9 @@
-#include "std_lib_facilities.h"
+//#include "std_lib_facilities.h"
+#include <iostream>
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	using namespace std;
 	cout << "Текстовая строка\n";
 	cout << "Еще одна текстовая строка\n";
 	cin.get();
